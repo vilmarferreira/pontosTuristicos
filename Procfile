7bfbb88256e5c6729ceffa488ee1ee7a38c51c31
Vilmar Ferreira <ferreiragomes7792@gmail.com>
2018-05-10 10:39:31 -0300

    Configuring the app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pontosTuristicos.wsgi --log-file -
--- pontosTuristicos/settings.py
+++ pontosTuristicos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1c3p-ee9nmc%3z*ocxnb$dzr08%_)u%e53)&oe%to!4i6i89!h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vilmar-pontos-turisticos.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
@@ -82,12 +83,13 @@ WSGI_APPLICATION = 'pontosTuristicos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from dj_database_url import parse as dburl
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
+
+
 
 
 # Password validation
@@ -129,6 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 REST_FRAMEWORK = {
